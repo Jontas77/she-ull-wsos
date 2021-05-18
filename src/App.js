913850391e0e2b644bb64d9f8f12ffdcae663ad4
Jonathan Heugh <mrjbheugh@gmail.com>
2021-05-18 15:14:32 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { Button, Form, Container, Header } from "semantic-ui-react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      "Leerder Naam": "",
+      "Jaar Groep": "",
+      Oortreding: "",
+      "Datum van Oortreding": "",
+      Opvoerder: "",
+      Sanksie: "",
+      "Sanksie Datum": "",
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+
+    axios
+      .post(
+        "https://sheet.best/api/sheets/aa636ff6-a777-4abb-a8df-b66265a8778f",
+        this.state
+      )
+      .then((response) => {
+        console.log(response);
+      });
+    e.target.reset();
+  };
+
+  render() {
+    const {
+      naam,
+      groep,
+      oortreding,
+      datumVanOortreding,
+      opvoerder,
+      sanksie,
+      sanksieDatum,
+    } = this.state;
+
+    return (
+      <Container fluid className="container">
+        <Header as="h2">WSOS Leerder Oortreding Register</Header>
+
+        <Form
+          className="form"
+          onSubmit={this.submitHandler}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Form.Field>
+            <label>Naam van leerder</label>
+            <input
+              placeholder="Leerder se naam en van"
+              type="text"
+              name="Leerder Naam"
+              value={naam}
+              onChange={this.changeHandler}
+            />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Jaar Groep</label>
+            <input
+              placeholder="Jaar groep"
+              type="number"
+              name="Jaar Groep"
+              value={groep}
+              onChange={this.changeHandler}
+            />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Oortreding</label>
+            <input
+              placeholder="Oortreding"
+              type="text"
+              name="Oortreding"
+              value={oortreding}
+              onChange={this.changeHandler}
+            />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Datum van Oortreding</label>
+            <input
+              type="date"
+              name="Datum van Oortreding"
+              value={datumVanOortreding}
+              onChange={this.changeHandler}
+            />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Opvoerder</label>
+            <input
+              placeholder="Opvoerder"
+              type="text"
+              name="Opvoerder"
+              value={opvoerder}
+              onChange={this.changeHandler}
+            />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Sanksie</label>
+            <input
+              placeholder="Sanksie"
+              type="text"
+              name="Sanksie"
+              value={sanksie}
+              onChange={this.changeHandler}
+            />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Sanksie Datum</label>
+            <input
+              type="date"
+              name="Sanksie Datum"
+              value={sanksieDatum}
+              onChange={this.changeHandler}
+            />
+          </Form.Field>
+
+          <Button color="blue" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 export default App;
